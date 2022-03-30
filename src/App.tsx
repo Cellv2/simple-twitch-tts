3204b65fbd6e802e18a09b731c00a3dc8e28ddd4
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2022-03-30 20:47:57 +0100

    Switch to artyom.js to handle speech

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,14 @@
-import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
+// https://docs.ourcodeworld.com/projects/artyom-js
+// @ts-expect-error - frustratingly this doesn't work well with TS
+import Artyom from "artyom.js";
+import React, { useState } from "react";
+import tmi from "tmi.js";
 import "./App.css";
 
-import tmi from "tmi.js";
-
-import Queue from "./classes/queue";
-
-const queue = new Queue();
+const artyom = new Artyom();
 
 const App = () => {
-    const synth = window.speechSynthesis;
-    const voices = synth.getVoices();
-
-    const utterance = new SpeechSynthesisUtterance();
-    utterance.voice = voices[5];
-    utterance.volume = 0.5;
-    utterance.rate = 1;
-    utterance.pitch = 1;
-    // utterance.text = message;
-    utterance.lang = "";
-
-    const speak = (message: string) => {
-        utterance.text = message;
-        synth.speak(utterance);
-    };
+    const [channelName, setChannelName] = useState<string>("");
 
     // https://tmijs.com/
     const client = new tmi.Client({
@@ -31,107 +16,63 @@ const App = () => {
             secure: true,
             reconnect: true,
         },
-        channels: ["loltyler1"],
+        channels: [channelName],
     });
 
-    client.connect();
-
-    client.on("message", (channel, tags, message, self) => {
-        if (self) {
-            return;
+    const connectClient = () => {
+        if (!channelName.length) {
+            console.error("no channel set");
         }
 
-        // had issues with undefined somehow getting sent?
-        if (!message.length) {
-            return;
-        }
+        console.log("connecting client");
+        client.connect().then(() => console.log(`connected to ${channelName}`));
 
-        console.log(`${tags["display-name"]}: ${message}`);
+        client.on("message", (channel, tags, message, self) => {
+            if (self) {
+                return;
+            }
 
-        // speak(message);
+            // had issues with undefined somehow getting sent?
+            if (!message.length) {
+                return;
+            }
 
-        utterance.text = message;
-        synth.speak(utterance);
-
-        // queue.enqueue(message);
-
-        // speak(message);
-    });
-
-    // const initClient = () => {
-    //     // we need to interact with the page to load the voices I believe
-    //     // maybe can write a function to load them if they are not there?
-    //     // otherwise just an initialisation for the tmi client should be enough (probs need that anyway)
-
-    //     // https://tmijs.com/
-    //     const client = new tmi.Client({
-    //         connection: {
-    //             secure: true,
-    //             reconnect: true,
-    //         },
-    //         channels: [channelName],
-    //     });
-
-    //     client.connect();
-
-    //     client.on("message", (channel, tags, message, self) => {
-    //         if (self) {
-    //             return;
-    //         }
-
-    //         console.log(`${tags["display-name"]}: ${message}`);
-
-    //         queue.enqueue(message);
-
-    //         // speak(message);
-    //     });
-    // };
-
-    let isProcessing = false;
-    const startQueue = () => {
-        console.log("checking queue");
-        if (!isProcessing) {
-            isProcessing = true;
-            do {
-                speak(queue.dequeue());
-            } while (queue.length());
-        }
-
-        isProcessing = false;
+            // console.log(`${tags["display-name"]}: ${message}`);
+            artyom.say(message);
+        });
     };
 
-    const [channelName, setChannelName] = useState<string>("");
+    const disconnectClient = () => {
+        client.disconnect();
 
-    // const delay = 5000;
-    // useEffect(() => {
-    //     console.log("we started checking the queue again");
-    //     let timer = setInterval(startQueue, delay);
-    // }, []);
+        // stops speech just terminating half way through a sentence - not sure if this is a good idea ?
+        // https://docs.ourcodeworld.com/projects/artyom-js/documentation/methods/when
+        artyom.when("SPEECH_SYNTHESIS_END", () => {
+            artyom.shutUp();
+            artyom.clearGarbageCollection();
+        });
+
+        console.log(`disconnected from ${channelName}`);
+    };
 
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+                <input
+                    type="text"
+                    name=""
+                    id="channelName"
+                    onChange={(val) => setChannelName(val.target.value)}
+                    value={channelName}
+                />
                 <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
+                    ENSURE THE BELOW HAS VALIDATION THAT THERE IS A CHANNEL NAME
                 </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <button onClick={connectClient}>connect me KEKW</button>
+                <button onClick={disconnectClient}>
+                    get me the hell out of here
+                </button>
             </header>
-            <input
-                type="text"
-                name=""
-                id="channelName"
-                onChange={(val) => setChannelName(val.target.value)}
-                value={channelName}
-            />
-            {/* <button onClick={initClient}>connect me KEKW</button> */}
         </div>
     );
 };
